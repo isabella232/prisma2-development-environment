495dc3514afeff0b673b11ba6587215f49fc0b51
timsuchanek <tim.suchanek@gmail.com>
2020-01-23 12:09:00 +0100

    add commit env var

--- .buildkite/publish/buildkite-entry.sh
+++ .buildkite/publish/buildkite-entry.sh
@@ -1,4 +1,12 @@
+set -ex
+
 echo "BUILDKITE_TAG"
 echo $BUILDKITE_TAG
+
+if [ -v $DEVELOPMENT_ENVIRONMENT_COMMIT ]; then
+  git checkout $DEVELOPMENT_ENVIRONMENT_COMMIT
+fi
+
+
 
 buildkite-agent pipeline upload .buildkite/publish/publish.yml
