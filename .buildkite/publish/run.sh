6a9d0ca0f6d60ae7a0c193789a255240f17ad61e
Joël <joel.galeran@gmail.com>
2020-03-09 15:32:04 +0100

    Remove node-gyp

--- .buildkite/publish/run.sh
+++ .buildkite/publish/run.sh
@@ -7,7 +7,6 @@ if [ "$DEVELOPMENT_ENVIRONMENT_COMMIT" ]; then
   git checkout $DEVELOPMENT_ENVIRONMENT_COMMIT
 fi
 
-npm i -g node-gyp
 npm i -g pnpm@4.9.3
 npm i -g yarn || echo "Ok"
 yarn --version || echo "Ok"
