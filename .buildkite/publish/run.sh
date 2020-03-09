13ced03207d231001af5e62da1ba16d14349a61f
Joël <joel.galeran@gmail.com>
2020-03-09 15:05:02 +0100

    Add node-gyp for better-sqlite3 dependency in integration tests

--- .buildkite/publish/run.sh
+++ .buildkite/publish/run.sh
@@ -7,6 +7,7 @@ if [ "$DEVELOPMENT_ENVIRONMENT_COMMIT" ]; then
   git checkout $DEVELOPMENT_ENVIRONMENT_COMMIT
 fi
 
+npm i -g node-gyp
 npm i -g pnpm@4.9.3
 npm i -g yarn || echo "Ok"
 yarn --version || echo "Ok"
