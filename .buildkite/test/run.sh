0d4dd718eff4fa1c4744c899a023199df146511f
Joël <joel.galeran@gmail.com>
2020-04-07 16:47:15 +0200

    Fix pnpm i sqlite3

--- .buildkite/test/run.sh
+++ .buildkite/test/run.sh
@@ -8,7 +8,7 @@ pnpm i --no-prefer-frozen-lockfile
 pnpm run setup
 
 cd prisma/cli/prisma2
-pnpm i --no-prefer-frozen-lockfile sqlite3@4.1.1 --unsafe-perm
+pnpm i sqlite3@4.1.1 --unsafe-perm
 cd ../../..
 
 pnpm run test
